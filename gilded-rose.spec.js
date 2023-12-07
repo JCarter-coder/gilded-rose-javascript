8eb16c616ad63bb3e7257791685eb38b8e82327c
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-07 16:04:30 -0700

    "practice test"

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -11,4 +11,13 @@ describe("updateQuality", () => {
     expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
   });
+  it("Sulfuras, Hand of Ragnaros does not change", () => {
+    const testItem = new Item("Sulfuras, Hand of Ragnaros", 1, 80);
+    items.push(testItem);
+
+    updateQuality();
+
+    expect(testItem.quality).toBe(80);
+    expect(testItem.sellIn).toBe(1);
+  });
 });
